1060a62456e24fd6d9d58d46f7a5a9cc7f2844d9
Julian Seguel <julian.seguel@servexternos.santander.cl>
2023-03-14 16:51:18 -0300

    Se corrige la ruta del root

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use( express.json() );
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.get('*', ( req, res ) => {
+  res.sendFile( __dirname + '/public/index.html' );
+})
 // app.get('/', (req, res) => {
 //   res.json({
 //     ok: true
